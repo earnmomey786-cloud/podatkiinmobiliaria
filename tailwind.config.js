757304c008054754507e23694f5749b197739654
Tabeer Ashi <SDelmary@my.gcu.edu>
2025-09-18 15:38:54 +0200

    añadir proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'serif': ['Playfair Display', 'serif'],
+        'sans': ['Inter', 'sans-serif'],
+      },
+      colors: {
+        bronze: {
+          500: '#917a52',
+          600: '#7d6847',
+        }
+      }
+    },
+  },
+  plugins: [],
+};
